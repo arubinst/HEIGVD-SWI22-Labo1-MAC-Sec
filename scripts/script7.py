a6456506b577032a3940a5a2a438580e569449f9
Meridas <n.viotti@hotmail.com>
2022-03-31 15:57:45 +0200

    Exo bonus

---
+++ scripts/script7.py
@@ -0,0 +1,24 @@
+# Auteurs : Peguiron Adrien, Viotti Nicolas
+
+#Largement (complètement) inspiré par le maître Vivek Ramachandran, https://www.youtube.com/watch?v=_OpmfE43AiQ
+
+#!/bin/python
+
+import socket
+import scapy
+
+IFACE = "wlan0" #Interface à utiliser
+
+hidden_ssid_aps = set()
+
+def PacketHandler(pkt) :
+    if pkt.haslayer(Dot11Beacon):
+        if not pkt.info : #Si le wifi n'a pas de SSID,  donc une longueur de 0, on peut donc définir qu'il est "caché"
+            if pkt.addr3 not in hidden_ssid_aps:
+                hidden_ssid_aps.add(pkt.addr3)
+                print "Réseau invisible trouvé ! BSSID:", pkt.addr3
+
+        elif pkt.haslayer(Dot11ProbeResp) and (pkt.addr3 in hidden_ssid_aps): # On cherche à récupérer le nom d'un réseau caché déjà rencontré
+            print "SSID découvert: ", pkt.info, pkt.add3
+
+sniff(prn=PacketHandler, iface=IFACE, timeout=10)
