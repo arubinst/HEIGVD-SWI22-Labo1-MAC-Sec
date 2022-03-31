57e8e39a425b0cdc05290604f818360bdb5eb838
Delphine Scherler <delphine.scherler@heig-vd.ch>
2022-03-31 22:52:52 +0200

    update hidden SSID script

--- Part02/hidden_SSID.py
+++ Part02/hidden_SSID.py
@@ -1,33 +1,51 @@
 # Authors : Delphine Scherler & Wenes Limem
 # Date : 31.03.2022
-# Source : https://www.researchgate.net/publication/356557247_Discovering_Hidden_Wireless_Networks_leverazing_Python_Scapy
-# Source : https://gist.github.com/dropmeaword/42636d180d52e52e2d8b6275e79484a0
+# Description : Script capable de révéler le SSID correspondant à un réseau configuré comme étant "invisible".
+
+# Remarque : Pour l'instant le script détecte les réseaux cachés, mais il manque la révélation du SSID
 
 from scapy.all import *
-from scapy.layers.dot11 import Dot11Beacon, Dot11
+from scapy.layers.dot11 import Dot11Beacon, Dot11, Dot11Elt, Dot11ProbeReq
+
+bssid = ""
+probes = {}
 
 
+# find probes
+def find_probes(pkt):
+    if pkt.haslayer(Dot11ProbeReq):
+        # extract the MAC address of the network
+        essid = pkt[Dot11Elt].info
+        # get the name of it
+        ssid = pkt[Dot11Elt].info.decode()
+        # extract network stats
+        stats = pkt[Dot11ProbeReq].network_stats()
+        # get the channel of the AP
+        channel = stats.get("channel")
+        # add probe to the list
+        if essid not in probes:
+            probes[essid] = [essid, ssid, channel, pkt.info]
+
+
+# method to find hidden Wifi
 def hidden_ap_discovery(pkt):
-    bssid=0
     if pkt.haslayer(Dot11Beacon):
         # if packet does not show SSID
-        if not pkt.info:
+        if '\x00' in pkt[Dot11Elt].info.decode():
             bssid = pkt.addr2
-            print("Hidden Wifi detected ! BSSID: " + bssid)
-
-    # if we found a hidden Wifi
-    if bssid:
-        # find probe requets with BSSID and display SSID
-        if pkt.haslayer(Dot11):
-            # subtype 4 = Probe Requests
-            if pkt.type == 0 and pkt.subtype == 4:
-                currentMac = pkt.addr2
-                if currentMac == bssid:
-                    print("SSID of hidden Wifi: " + pkt.info)
+            print("Hidden Wifi detected! BSSID: " + bssid)
 
 
 if __name__ == "__main__":
     interface = "wlan0"
-    t_out=5
-    sniff(iface=interface, prn=hidden_ap_discovery,timeout=5)
+    t_out = 5
+    print("sniffing for hidden wifi during " + str(t_out) + " seconds")
+    sniff(iface=interface, prn=hidden_ap_discovery, timeout=t_out)
+    sniff(iface=interface, prn=find_probes, timeout=t_out)
     print("done sniffing ...\n")
+
+    print(probes)
+
+
+
+
