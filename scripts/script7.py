1268b5bcb0cf822f9cae9aee7841fab048cdc2fe
Meridas <n.viotti@hotmail.com>
2022-03-31 16:32:26 +0200

    Update script7.py

--- scripts/script7.py
+++ scripts/script7.py
@@ -13,12 +13,12 @@ hidden_ssid_aps = set()
 
 def PacketHandler(pkt) :
     if pkt.haslayer(Dot11Beacon):
-        if pkt.info == b'\x00\x00\x00\x00\x00' : #Si le wifi n'a pas de SSID,  donc une longueur de 0, on peut donc définir qu'il est "caché"
+        if pkt.info == b'\x00\x00\x00\x00\x00' : #Si le wifi n'a pas de SSID,  donc une longueur de 4 bytes sous forme de 0x00, on peut donc définir qu'il est "caché"
             if pkt.addr3 not in hidden_ssid_aps:
                 hidden_ssid_aps.add(pkt.addr3)
                 print("Réseau invisible trouvé ! BSSID: " + pkt.addr3)
 
-        elif pkt.haslayer(Dot11ProbeResp) and (pkt.addr3 in hidden_ssid_aps): # On cherche à récupérer le nom d'un réseau caché déjà rencontré
+        elif pkt.haslayer(Dot11ProbeResp) and (pkt.addr3 in hidden_ssid_aps): # On cherche à récupérer le nom d'un réseau caché déjà rencontré, grâce à la réponse de l'AP
             print("SSID découvert: " + pkt.info + " " + pkt.add3)
 
 sniff(prn=PacketHandler, iface=IFACE, timeout=10)
