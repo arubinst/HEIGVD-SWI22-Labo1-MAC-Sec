471d4d5978958dd27b9a39b0676b131af852c978
Thierry <adrien.peguiron@heig-vd.ch>
2022-03-31 16:27:31 +0200

    script7 ok

--- scripts/script7.py
+++ scripts/script7.py
@@ -4,8 +4,8 @@
 
 #!/bin/python
 
+from scapy.all import *
 import socket
-import scapy
 
 IFACE = "wlan0" #Interface à utiliser
 
@@ -13,12 +13,12 @@ hidden_ssid_aps = set()
 
 def PacketHandler(pkt) :
     if pkt.haslayer(Dot11Beacon):
-        if not pkt.info : #Si le wifi n'a pas de SSID,  donc une longueur de 0, on peut donc définir qu'il est "caché"
+        if pkt.info == b'\x00\x00\x00\x00\x00' : #Si le wifi n'a pas de SSID,  donc une longueur de 0, on peut donc définir qu'il est "caché"
             if pkt.addr3 not in hidden_ssid_aps:
                 hidden_ssid_aps.add(pkt.addr3)
-                print "Réseau invisible trouvé ! BSSID:", pkt.addr3
+                print("Réseau invisible trouvé ! BSSID: " + pkt.addr3)
 
         elif pkt.haslayer(Dot11ProbeResp) and (pkt.addr3 in hidden_ssid_aps): # On cherche à récupérer le nom d'un réseau caché déjà rencontré
-            print "SSID découvert: ", pkt.info, pkt.add3
+            print("SSID découvert: " + pkt.info + " " + pkt.add3)
 
 sniff(prn=PacketHandler, iface=IFACE, timeout=10)
