0cc331a0d45400873107f60b57888f68ecdbed23
Meridas <n.viotti@hotmail.com>
2022-03-31 15:01:26 +0200

    Comments

--- scripts/script6.py
+++ scripts/script6.py
@@ -1,3 +1,5 @@
+# Auteurs : Peguiron Adrien, Viotti Nicolas
+
 from scapy.all import *
 from threading import Thread
 import pandas
@@ -9,16 +11,15 @@ staAddr = []
 staAndApp = []
 
 def callback(packet):
-    # Detection des APs
+    # Détection des APs
     if packet.haslayer(Dot11Beacon):
-        # extract the MAC address of the network
+        # Extrait l'adresse MAC du réseau
         if packet[Dot11].addr3 not in apAddr:
-            #print(packet[Dot11].info.decode() + packet[Dot11].addr3)
             apAddr.append(packet[Dot11].addr3)
 
 def callback2(packet):
-    if packet.haslayer(Dot11QoS) and packet[Dot11].addr1 != "ff:ff:ff:ff:ff:ff":
-        if packet[Dot11].addr3 in apAddr:
+    if packet.haslayer(Dot11QoS) and packet[Dot11].addr1 != "ff:ff:ff:ff:ff:ff": # on vérifie que ce ne soit pas un message broadcast
+        if packet[Dot11].addr3 in apAddr: 
             sap = "La STA " + packet[Dot11].addr2 + " est connectée à l'AP " + packet[Dot11].addr3
             if sap not in staAndApp:
                 staAndApp.append(sap)
@@ -26,21 +27,21 @@ def callback2(packet):
             staAddr.append(packet[Dot11].addr2)
 
 def main():
-    iface = "wlan0"
+    IFACE = "wlan0" # Interface à utiliser
 
-    sniff(prn=callback, iface=iface, timeout=10)
-    sniff(prn=callback2, iface=iface, timeout=10)
+    sniff(prn=callback, iface=IFACE, timeout=10)
+    sniff(prn=callback2, iface=IFACE, timeout=10)
     print("APs disponibles : ")
-    for a in apAddr:
-        print (a)
+    for ap in apAddr:
+        print (ap)
 
     print("STAs disponibles : ")
-    for s in staAddr:
-        print(s)
+    for sta in staAddr:
+        print(sta)
 
     print("Connexions : ")
-    for t in staAndApp:
-        print(t)
+    for sap in staAndApp:
+        print(sap)
 
 if __name__ == "__main__":
     main()
